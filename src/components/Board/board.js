82e4b3707c99563dd6086fd2264aa768accd0660
ansarimoiz979 <ansarimoiz979@gmail.com>
2023-04-24 13:03:29 +0530

    board component and box component defined.

---
+++ src/components/Board/board.js
@@ -0,0 +1,11 @@
+import './board.css';
+import Box from '../Box/box';
+function App() {
+  return (
+    <div className="App">
+        <Box/>
+    </div>
+  );
+}
+
+export default Board;
---
+++ src/components/Box/box.js
@@ -0,0 +1,10 @@
+import './box.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default Box;
