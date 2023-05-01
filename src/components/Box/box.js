96fc1e5ba84496a74a905c21fd61f7cb12e570e5
ansarimoiz979 <ansarimoiz979@gmail.com>
2023-05-01 23:35:41 +0530

    removing unwanted changes

--- src/components/Box/box.js
+++ src/components/Box/box.js
@@ -3,15 +3,14 @@ import Xlogo from "./XImage.png";
 import Ologo from "./OImage.png";
 
 function Box(props) {
-    console.log("value",props.value);
+    console.log("value", props.value);
     return (
         <div className='each-box' onClick={props.actionClick}>
-            {/* <h5>{props.value}</h5> */}
             {
-                ( props.value != null ) && (
-                ( props.value == 1) ? 
-                <img src={Xlogo} alt="not found" width="100px"/> :
-                <img src={Ologo} alt="not found1" width="100px"/>
+                (props.value != null) && (
+                    (props.value == 1) ?
+                        <img src={Xlogo} alt="not found" width="100px" /> :
+                        <img src={Ologo} alt="not found1" width="100px" />
                 )
 
             }
